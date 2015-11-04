49c2516e448a7a6459a9981f4d05da0bd146bdf4
Vadim Ogievetsky <vadimon@gmail.com>
2015-11-03 18:44:26 -0800

    updated example

--- docs/examples/tonic.js
+++ docs/examples/tonic.js
@@ -1,38 +1,54 @@
-var plywood = require('../../build/plywood');
+var plywood = require('plywood');
 var ply = plywood.ply;
 var $ = plywood.$;
 var External = plywood.External;
 
-// Define an external
-var context = {
-  wiki: External.fromJS({
-    engine: 'druid',
-    dataSource: 'wikipedia',  // The datasource name in Druid
-    timeAttribute: 'time',  // Druid's anonymous time attribute will be called 'time'
-    attributes: [
-      { name: 'time', type: 'TIME' },
-      { name: 'page', type: 'STRING' },
-      { name: 'added', type: 'NUMBER' }
-    ]
-  })
-};
+// Define an external or two
+var mysqlExternal = External.fromJS({
+  engine: 'mysql',
+  table: 'wikipedia',
+  attributes: [
+    { name: 'time', type: 'TIME' },
+    { name: 'page', type: 'STRING' },
+    { name: 'language', type: 'STRING' },
+    { name: 'added', type: 'NUMBER' }
+  ]
+});
+
+var druidExternal = External.fromJS({
+  engine: 'druid',
+  dataSource: 'wikipedia',
+  timeAttribute: 'time',
+  attributes: [
+    { name: 'time', type: 'TIME' },
+    { name: 'page', type: 'STRING' },
+    { name: 'language', type: 'STRING' },
+    { name: 'added', type: 'NUMBER' }
+  ]
+});
 
 // Create an expression
 var ex = ply()
   .apply("wiki",
     $('wiki').filter(
-      $("time").in({ start: new Date("2015-08-26T00:00:00Z"), end: new Date("2015-08-27T00:00:00Z") })
-        .and($('language').is('en'))
+      $("time").in({
+        start: new Date("2015-08-26T00:00:00Z"),
+        end: new Date("2015-08-27T00:00:00Z")
+      })
+        .and($('language').in(['English', 'Spanish']))
     )
   )
-  .apply('Count', $('wiki').count())
   .apply('TotalAdded', '$wiki.sum($added)')
   .apply('Pages',
     $('wiki').split('$page', 'Page')
       .apply('TotalAdded', '$wiki.sum($added)')
-      .sort('$Count', 'descending')
+      .sort('$TotalAdded', 'descending')
       .limit(6)
   );
 
-// Let's see what queries this would make
-console.log(ex.simulateQueryPlan(context));
+// Let's see what queries this would make against these databases
+console.log("The query plans are:");
+console.log(ex.simulateQueryPlan({ wiki: mysqlExternal }));
+console.log(ex.simulateQueryPlan({ wiki: druidExternal }));
+
+'Fin.';
