d297a2a9fc588fb45373621342825584b5185b2e
eva <evaliou@imply.io>
2016-08-03 10:30:21 -0700

    revert info.js

--- test/info.js
+++ test/info.js
@@ -16,16 +16,16 @@
 
 const DOCKER_MACHINE = '192.168.99.100';
 
-exports.druidVersion = '0.9.1-rc4';
+exports.druidVersion = '0.9.1';
 exports.druidHost = `${DOCKER_MACHINE}:8082`;
 
-exports.mySqlVersion = '5.7.12';
+exports.mySqlVersion = '5.7.13';
 exports.mySqlHost = `${DOCKER_MACHINE}:3306`;
 exports.mySqlDatabase = 'datazoo';
 exports.mySqlUser = 'root';
 exports.mySqlPassword = '';
 
-exports.postgresVersion = '9.5.3';
+exports.postgresVersion = '9.5.2';
 exports.postgresHost = `${DOCKER_MACHINE}:5432`;
 exports.postgresDatabase = 'datazoo';
 exports.postgresUser = 'root';
